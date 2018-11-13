b5fd1b481f64928393dd18723b47d766fcd0df51
Maeve <mfoley@cshnyc.org>
2018-11-13 11:56:33 -0500

    Update tswift_read_txt.py

--- tswift_read_txt.py
+++ tswift_read_txt.py
@@ -1,9 +1,21 @@
-count = 0
+# count = 0
+#f = open("all_tswift_lyrics.txt","r")
+# for line in f:
+#     words = line.split()
+#     for x in words:
+#         if x == "love":
+#             count = count+1
+# print("Love appears", count , "times.")
+# f.close()
+
+from collections import Counter
 f = open("all_tswift_lyrics.txt","r")
+
 for line in f:
     words = line.split()
     for x in words:
-        if x == "love":
-            count = count+1
-print("Love appears", count , "times.")
+        #print(words)
+        x = Counter(words)
+        print(x)
+
 f.close()
